25a07c59b24f5e6fc44729304ccd8e32532ba254
ak-matsu <sandbox.matsu@gmail.com>
2021-05-02 16:29:32 +0900

    寄付には必ずログインを求めるよう設定

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,4 +1,9 @@
 class DonationsController < ApplicationController
+  # ログインしていない場合はログインページに遷移するように設定
+  before_action :authenticate_user!, except: :index
   def index
+  end
+
+  def new
   end
 end
